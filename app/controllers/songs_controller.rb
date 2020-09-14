1931ab4ec9498cc7f1d2a069c21b369dfe08d77c
JacksonReynolds <jwreynod@gmail.com>
2020-09-14 12:28:43 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,6 +9,8 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @genres = Genre.all
+    @notes = Note.new, Note.new
   end
 
   def create
@@ -47,7 +49,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :notes[])
   end
 end
 
