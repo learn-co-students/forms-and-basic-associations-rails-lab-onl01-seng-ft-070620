0dad935abb485fd2320b4ae0ab09cc452c8f8104
jchasse <jonathan.r.chasse@gmail.com>
2020-09-17 14:10:11 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,11 @@
 class Genre < ActiveRecord::Base
-  # add associations
+  has_many :songs
+
+  # def song_title=(title)
+  #   self.song = Song.find_or_create_by(title: title)
+  # end
+ 
+  # def song_title
+  #    self.song ? self.song.title : nil
+  # end
 end
